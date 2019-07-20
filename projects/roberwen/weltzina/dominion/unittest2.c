edc19632e06ac02987bd1dff49b74dedfe783909
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-20 10:14:28 -0700

    Update unittest2.c

--- projects/roberwen/weltzina/dominion/unittest2.c
+++ projects/roberwen/weltzina/dominion/unittest2.c
@@ -57,8 +57,8 @@ int main() {
 	choice1 = 0;
 	cardEffect(baron, choice1, choice2, choice3, &testG, handpos, &bonus);
 
-	buyincrease = 1;
-	estateincrease = 1;
+	int buyincrease = 1;
+	int estateincrease = 1;
 	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
 	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
 	printf("coins = %d, expected = %d\n", testG.coins, G.coins + xtraCoins);
