fa24d4f74ce75daf713421ed1d51d207c1eabacb
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-20 10:27:59 -0700

    Update unittest2.c

--- projects/roberwen/weltzina/dominion/unittest2.c
+++ projects/roberwen/weltzina/dominion/unittest2.c
@@ -63,7 +63,7 @@ int main() {
 	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
 	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
 	printf("coins = %d, expected = %d\n", testG.coins, G.coins + xtraCoins);
-	//assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
+	assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
 	assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
 	assert(testG.coins == G.coins + xtraCoins);
   /*
