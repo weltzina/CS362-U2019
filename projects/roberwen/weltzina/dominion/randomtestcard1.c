15789adf0c123ca066dabb33f84cc52e211fb8f5
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-25 20:51:42 -0700

    Update randomtestcard1.c

--- projects/roberwen/weltzina/dominion/randomtestcard1.c
+++ projects/roberwen/weltzina/dominion/randomtestcard1.c
@@ -25,14 +25,14 @@ int checkPlayBaron(int choice1, struct gameState *state, int currentPlayer){
 
   int r;
   int card_not_discarded = 1;
-
-  pre.numBuys++;
-
-  r = playbaron(choice1, state, currentPlayer);
+  int infinite = 1;
 
   if(choice1 > 0){
       for(int i = 0; i < pre.handCount[currentPlayer]; i++){
         if(pre.hand[currentPlayer][i] == estate){
+          if(pre.hand[currentPlayer][i] == estate && i != 0){
+            infinite = 0;
+          }
           card_not_discarded = 0;
           pre.coins +=4;
         }
@@ -50,15 +50,23 @@ int checkPlayBaron(int choice1, struct gameState *state, int currentPlayer){
       pre.discardCount[currentPlayer]++;
       pre.supplyCount[estate]--;
     }
+    infinite = 0;
+  }
+  pre.numBuys++;
+
+  if(!infinite){
+    r = playbaron(choice1, state, currentPlayer);
+
+    if(!assertTrue(memcmp(&pre, state, sizeof(struct gameState)), 0) || r != 0){
+        printf("Estate SupplyCount = %d, expected %d\n", state->supplyCount[estate], pre.supplyCount[estate]);
+        printf("DiscardCount = %d, expected %d\n", state->discardCount[state->whoseTurn], pre.discardCount[currentPlayer]);
+        printf("Coins = %d, expected %d\n", state->coins, pre.coins);
+        printf("Buys = %d, expected %d\n", state->numBuys, pre.numBuys);
+    }
+  }else{
+    printf("FALSE--------------------------------------------------------------\nInfinite Loop\n");
   }
 
-
-  if(!assertTrue(memcmp(&pre, state, sizeof(struct gameState)), 0) || r != 0){
-      printf("Estate SupplyCount = %d, expected %d\n", state->supplyCount[estate], pre.supplyCount[estate]);
-      printf("DiscardCount = %d, expected %d\n", state->discardCount[state->whoseTurn], pre.discardCount[currentPlayer]);
-      printf("Coins = %d, expected %d\n", state->coins, pre.coins);
-      printf("Buys = %d, expected %d\n", state->numBuys, pre.numBuys);
-  }
   return 0;
 
 }
