0d570c6c2f29db5f2fc172ad18da9de135b2ac04
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-26 13:12:06 -0700

    Update dominion.c

--- projects/roberwen/weltzina/dominion/dominion.c
+++ projects/roberwen/weltzina/dominion/dominion.c
@@ -1154,7 +1154,7 @@ int playMinion(int choice1, int choice2, struct gameState *state, int currentPla
   int j;
 
   //+1 action
-  state->numActions++;
+/*  state->numActions++;
 
   //discard card from hand
   discardCard(handPos, currentPlayer, state, 0);
@@ -1163,15 +1163,14 @@ int playMinion(int choice1, int choice2, struct gameState *state, int currentPla
   {
     state->coins = state->coins + 2;
   }
-
-  else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+  else*/ if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
   {
     //discard hand
-  /*  while(numHandCards(state) < 0) // was >. Will not end up discarding and will still draw 4.**************************** Bug
+    while(numHandCards(state) < 0) // was >. Will not end up discarding and will still draw 4.**************************** Bug
     {
       discardCard(handPos, currentPlayer, state, 0);
     }
-*/
+
     //draw 4
     for (i = 0; i < 4; i++)
     {
