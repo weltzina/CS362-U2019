24bbfdd5f0d542f4c2090b60be61f52dd8d4c855
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-25 22:44:55 -0700

    Update randomtestcard1.c

--- projects/roberwen/weltzina/dominion/randomtestcard1.c
+++ projects/roberwen/weltzina/dominion/randomtestcard1.c
@@ -85,12 +85,12 @@ int main(){
   SelectStream(2);
   PutSeed(3);
 
-  for(int n = 0; n < 16; n++){
+  for(int n = 0; n < 200; n++){
     for(int i = 0; i < sizeof(struct gameState); i++){
       ((char*)&G)[i] = floor(Random() * 256);
     }
     G.numPlayers = floor(Random() * (MAX_PLAYERS-1))+2;
-  /*  G.whoseTurn = floor(Random() * G.numPlayers);
+    G.whoseTurn = floor(Random() * G.numPlayers);
     for(int j = 0; j < G.numPlayers; j++){
       G.deckCount[j] = floor(Random() * MAX_DECK * 1.2)-2;
       G.discardCount[j] = floor(Random() * MAX_DECK * 1.2)-2;
@@ -104,7 +104,7 @@ int main(){
       for(int f = 0; f < G.handCount[j]; f++){
         G.hand[j][f] = floor(Random() * treasure_map * 1.2)-2;
       }
-    }*/
+    }
     checkPlayBaron(floor(Random()*4)-1, &G, G.whoseTurn);
   }
 
