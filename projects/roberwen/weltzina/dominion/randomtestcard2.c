cefb47e06e8d19ae758c4f0eeee97c47a40890ed
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-26 16:19:07 -0700

    Update randomtestcard2.c

--- projects/roberwen/weltzina/dominion/randomtestcard2.c
+++ projects/roberwen/weltzina/dominion/randomtestcard2.c
@@ -89,7 +89,7 @@ int main(){
   SelectStream(2);
   PutSeed(3);
 
-  for(int n = 0; n < 2000; n++){
+  for(int n = 0; n < 1500; n++){
     for(int i = 0; i < sizeof(struct gameState); i++){
       ((char*)&G)[i] = floor(Random() * 256);
     }
