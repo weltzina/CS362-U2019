77e19656289ccfb3997531849b03c3ecf362807f
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-25 20:25:08 -0700

    Update randomtestcard1.c

--- projects/roberwen/weltzina/dominion/randomtestcard1.c
+++ projects/roberwen/weltzina/dominion/randomtestcard1.c
@@ -57,10 +57,26 @@ int checkPlayBaron(int choice1, struct gameState *state, int currentPlayer){
       printf("DiscardCount = %d, expected %d\n");
       printf("Coins = %d, expected %d\n");
       printf("Buys = %d, expected %d\n");
-      printf("state\n");
-      printf(state);
-      printf("expected\n");
-      printf(pre);
+
+      /*
+      int numPlayers; //number of players
+      int supplyCount[treasure_map+1];  //this is the amount of a specific type of card given a specific number.
+      int embargoTokens[treasure_map+1];
+      int outpostPlayed;
+      int outpostTurn;
+      int whoseTurn;
+      int phase;
+      int numActions; /* Starts at 1 each turn */
+      //int coins; /* Use as you see fit! */
+      //int numBuys; /* Starts at 1 each turn */
+      /*int hand[MAX_PLAYERS][MAX_HAND];
+      int handCount[MAX_PLAYERS];
+      int deck[MAX_PLAYERS][MAX_DECK];
+      int deckCount[MAX_PLAYERS];
+      int discard[MAX_PLAYERS][MAX_DECK];
+      int discardCount[MAX_PLAYERS];
+      int playedCards[MAX_DECK];
+      int playedCardCount;*/
   }
   return 0;
 
