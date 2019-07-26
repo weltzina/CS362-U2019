10e7a231b70660d9924c78c9b04c441aa75ff069
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-26 13:53:32 -0700

    swtch

--- projects/roberwen/weltzina/dominion/dominion.c
+++ projects/roberwen/weltzina/dominion/dominion.c
@@ -1278,7 +1278,7 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state, int tra
   else
     {
       //replace discarded card with last card in hand
-      state->hand[currentPlayer][handPos] = state->hand[currentPlayer][ (state->handCount[currentPlayer] - 1)];
+      state->hand[currentPlayer][handPos] = state->hand[currentPlayer][state->handCount[currentPlayer] - 1];
       //set last card to -1
       state->hand[currentPlayer][state->handCount[currentPlayer] - 1] = -1;
       //reduce number of cards in hand
--- projects/roberwen/weltzina/dominion/randomtestcard2.c
+++ projects/roberwen/weltzina/dominion/randomtestcard2.c
@@ -99,6 +99,7 @@ int main(){
       G.deckCount[j] = floor(Random() * MAX_DECK *1.2)-2;
       G.discardCount[j] = floor(Random() * MAX_DECK *1.2)-2;
       G.handCount[j] = floor(Random() * MAX_HAND *1.2)-2;
+      G.playedCardCount[j] = floor(Random() * MAX_HAND * 1.2)-2;
       for(int f = 0; f < G.deckCount[j]; f++){
         G.deck[j][f] = floor(Random() * treasure_map * 1.2)-2;
       }
@@ -107,6 +108,9 @@ int main(){
       }
       for(int f = 0; f < G.handCount[j]; f++){
         G.hand[j][f] = floor(Random() * treasure_map * 1.2)-2;
+      }
+      for(int f = 0; f < G.playedCardCount[j]; f++){
+        G.playedCards[j][f] = floor(Random() * treasure_map * 1.2)-2;
       }
     }
     checkPlayMinion(floor(Random() * 4)-1, floor(Random() * 4)-1, &G, G.whoseTurn, floor(Random() *G.handCount[G.whoseTurn]));
