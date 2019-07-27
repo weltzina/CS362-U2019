ee70e3f308d27da1f9724f4bb3bce5d7597db622
Alec Weltzin <weltzina@oregonstate.edu>
2019-07-26 20:41:36 -0700

    3 test dial

--- projects/roberwen/weltzina/dominion/randomtestcard2.c
+++ projects/roberwen/weltzina/dominion/randomtestcard2.c
@@ -10,10 +10,10 @@
 
 int assertTrue(int var1, int var2){
   if(var1 != var2){
-    /*printf("FALSE--------------------------------------------------------------\n");*/
+    printf("FALSE--------------------------------------------------------------\n");
     return 0;
   }else{
-    /*printf("TRUE---------------------------------------------------------------\n\n\n\n");*/
+    printf("TRUE---------------------------------------------------------------\n\n\n\n");
     return 1;
   }
 }
@@ -59,15 +59,15 @@ int checkPlayMinion(int choice1, int choice2, struct gameState *state, int curre
   r = playMinion(choice1, choice2, state, currentPlayer, handPos);
 
   if(!assertTrue(memcmp(&pre, state, sizeof(struct gameState)), 0)){
-  /*  printf("DiscardCount = %d, expected %d\n", state->discardCount[currentPlayer], pre.discardCount[currentPlayer]);
+    printf("DiscardCount = %d, expected %d\n", state->discardCount[currentPlayer], pre.discardCount[currentPlayer]);
     printf("Hand Count = %d, expected %d\n", state->handCount[currentPlayer], pre.handCount[currentPlayer]);
     printf("Deck Count = %d, expected %d\n", state->deckCount[currentPlayer], pre.deckCount[currentPlayer]);
-    printf("coins = %d, expected %d\n", state->coins, pre.coins);*/
+    printf("coins = %d, expected %d\n", state->coins, pre.coins);
     for(int q = 0; q < pre.numPlayers; q++){
       if(players[q] == 1){
-        /*printf("player %d discard = %d, expected %d\n", q, state->discardCount[q], pre.discardCount[q]);
+        printf("player %d discard = %d, expected %d\n", q, state->discardCount[q], pre.discardCount[q]);
         printf("player %d deck = %d, expected %d\n", q, state->deckCount[q], pre.deckCount[q]);
-        printf("player %d hand = %d, expected %d\n\n\n\n", q, state->handCount[q], pre.handCount[q]);*/
+        printf("player %d hand = %d, expected %d\n\n\n\n", q, state->handCount[q], pre.handCount[q]);
       }
     }
   }
